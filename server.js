a7068a13957c1b9f4ab2c6715fd4103cad7f010a
Xavier Bick <fxb9500@gmail.com>
2016-05-31 20:06:48 -0500

    First implementation of email API

--- server.js
+++ server.js
@@ -17,3 +17,30 @@ app.use('/api/v1/', router);
 
 app.listen(port);
 console.log("Express server running on port " + port);
+
+router.post('send-message', function(request, response) {
+  var sendgrid = require('sendgrid')('SENDGRID_API_KEY');
+
+  var payload = new sendgrid.Email({
+    to: 'fbick@udallas.edu',
+    from: request.params.fromAddress,
+    fromname: request.params.fromName,
+    subject: 'Message from ' + request.params.fromName + ' via Business Card',
+    text: request.params.message
+  });
+
+  var error = false;
+  sendgrid.send(email, function(err, json) {
+    if (err) { error = err; }
+    console.log(json);
+  });
+
+  if (error !== false) {
+    console.error(error);
+    response.status(500).json({ error: error });
+  }
+  else {
+    response.status(200).json({ message: "Email sent" });
+  }
+
+});
