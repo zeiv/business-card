1b655b92fa75d64707bafcbddcbfed35e4aa78a9
Xavier Bick <fxb9500@gmail.com>
2016-06-01 11:20:00 -0500

    Move ember to its own directory

--- app/transitions.js
+++
@@ -1,7 +0,0 @@
-export default function() {
-  this.transition(
-    this.fromRoute('index'),
-    this.use('cardFlip', { duration: 300, direction: 'right' } ),
-    this.reverse('cardFlip', { duration: 300, direction: 'left' } )
-  );
-}
