ec142167d4e04c089aefb17877c5e97c564de4bb
Xavier Bick <fxb9500@gmail.com>
2016-06-01 10:08:21 -0500

    Email API now working

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
+console.log("Sendgrid API Key: " + process.env.SENDGRID_API_KEY);
+var sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
+console.log(sendgrid);
+
 router.get('/', function(request, response) {
   response.json({ message: "Success" });
 });
@@ -18,15 +22,16 @@ app.use('/api/v1/', router);
 app.listen(port);
 console.log("Express server running on port " + port);
 
-router.post('send-message', function(request, response) {
-  var sendgrid = require('sendgrid')('SENDGRID_API_KEY');
+router.route('/send-message')
+.post(function(request, response) {
 
-  var payload = new sendgrid.Email({
+  var email = new sendgrid.Email({
     to: 'fbick@udallas.edu',
-    from: request.params.fromAddress,
-    fromname: request.params.fromName,
-    subject: 'Message from ' + request.params.fromName + ' via Business Card',
-    text: request.params.message
+    from: request.body.fromAddress,
+    replyto: request.body.fromAddress,
+    fromname: request.body.fromName,
+    subject: 'Message from ' + request.body.fromName + ' via Business Card',
+    text: request.body.message
   });
 
   var error = false;
@@ -35,11 +40,16 @@ router.post('send-message', function(request, response) {
     console.log(json);
   });
 
+  // if (email.text === '' || email.from === '') {
+  //   error = "Missing parameters";
+  // }
+
   if (error !== false) {
     console.error(error);
     response.status(500).json({ error: error });
   }
   else {
+    console.log(email);
     response.status(200).json({ message: "Email sent" });
   }
 
