1bfca46a6072002dab1ad607bf518c52ce7d1aa3
Xavier Bick <fxb9500@gmail.com>
2016-06-06 17:44:55 -0500

    Fix AJAX service host when environment in production

--- ember/app/services/ajax.js
+++ ember/app/services/ajax.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 import AjaxService from 'ember-ajax/services/ajax';
+import ENV from 'card/config/environment';
+
+var host;
+
+if (ENV.environment === 'production') {
+  host = undefined;
+} else {
+  host = 'http://localhost:8080';
+}
 
 export default AjaxService.extend({
   namespace: '/api/v1',
-  host: 'http://localhost:8080'
+  host: host
 });
