728dae9a0a9197d4c0119e449b34c9d2878379b4
Xavier Bick <fxb9500@gmail.com>
2017-09-20 15:37:51 -0500

    Fix Card Flip to be actually correct with cross-browser support

--- ember/app/transitions.js
+++ ember/app/transitions.js
@@ -2,8 +2,8 @@ export default function() {
   if($(window).width() >= 600) {
     this.transition(
       this.fromRoute('index'),
-      this.use('cardFlip', { duration: 300, direction: 'right' } ),
-      this.reverse('cardFlip', { duration: 300, direction: 'left' } )
+      this.use('cardFlip', { duration: 500, direction: 'right' } ),
+      this.reverse('cardFlip', { duration: 500, direction: 'left' } )
     );
   } else {
     this.transition(
--- ember/app/transitions/card-flip.js
+++ ember/app/transitions/card-flip.js
@@ -13,8 +13,18 @@ export default function cardFlip(opts={}) {
   //   this.newElement.closest('main').css('transform-origin', '50% 150%');
   // }
 
+  this.newElement.css('display', 'none');
+  this.newElement.css('transform', 'translateZ(1200px)');
+
   return Promise.all([
-    animate(this.oldElement, { translateZ: 1200, rotateY: 180*direction + 'deg' }, opts),
-    animate(this.newElement, { translateZ: [0, 1200], rotateY: ['0deg', -180*direction + 'deg'] }, opts)
-  ]);
+    // animate(this.oldElement, { translateZ: 1200, rotateY: 180*direction + 'deg' }, opts),
+    // animate(this.newElement, { translateZ: [0, 1200], rotateY: ['0deg', -180*direction + 'deg'] }, opts)
+    animate(this.oldElement, { translateZ: [900], rotateY: (90*direction + 'deg') }, opts),
+  ]).then( () => {
+    this.newElement.css('transform', `rotateY: ${-90*direction}deg`);
+    this.newElement.css('display', 'auto');
+    this.oldElement.css('display', 'none');
+    this.oldElement.css('transform', 'translateZ(0)');
+    animate(this.newElement, { translateZ: [0, 900], rotateY: ['0deg', (-90*direction + 'deg')] }, opts)
+  });
 }
